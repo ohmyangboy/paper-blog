b624e57b578b7856a5e5bb4ff9d8831c66c34cbb
ohmyangboy <ohmyangboy@gmail.com>
2026-08-29 13:00:16 +0800

    chore: update Formula sha256 to 2eb9343a

--- Formula/paper.rb
+++ Formula/paper.rb
@@ -4,7 +4,7 @@ class Paper < Formula
   desc "Minimal Markdown static site generator and writing CLI"
   homepage "https://github.com/ohmyangboy/paper-blog"
   url "https://github.com/ohmyangboy/paper-blog/archive/refs/tags/v0.1.3-beta.2.tar.gz"
-  sha256 "2d0756bc46c9264f377d215c3a3c05324f3e9df6c19687df164db6989a960a5a"
+  sha256 "2eb9343ac865028eff7c8bb4818ad4192526f0fd7b0712d3480f5a2103153e26"
   license "GPL-3.0-only"
 
   depends_on "python@3.12"
